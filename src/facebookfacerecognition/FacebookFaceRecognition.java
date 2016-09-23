45001374c86310d70d38d7ab75cdc4e5fd45dddb
Vinícius Almeida dos Santos <e.viniciusas@gmail.com>
2016-09-23 18:11:48 -0300

    Projeto NetBeans iniciado
    Criados alguns arquivos de teste de uso da biblioteca

---
+++ src/facebookfacerecognition/FacebookFaceRecognition.java
@@ -0,0 +1,19 @@
+package facebookfacerecognition;
+
+/**
+ *
+ * @author viniciusas
+ */
+public class FacebookFaceRecognition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, OpenCV");
+
+        new DetectFaceDemo().run();
+        
+    }
+    
+}
