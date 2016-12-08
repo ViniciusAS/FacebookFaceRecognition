7f9262b8e66a9e4f648b420dfed69de5a2354dfe
Vinícius Almeida dos Santos <e.viniciusas@gmail.com>
2016-12-08 17:22:03 -0200

    Installation fixes

--- installOpenCV_latest.sh
+++ installOpenCV_latest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+set -e
 sudo apt install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
-#git clone https://github.com/Itseez/opencv.git
-#git clone https://github.com/Itseez/opencv_contrib.git
+git clone https://github.com/Itseez/opencv.git
+git clone https://github.com/Itseez/opencv_contrib.git
 
 mkdir ./opencv/release/
 cd ./opencv/release/
@@ -14,6 +14,6 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
        ..
 
-make -j 7 # runs 7 jobs in parallel
+make -j 4 -k # -j 4: runs 4 jobs in parallel #  -k:  keep going
 
 sudo make install
